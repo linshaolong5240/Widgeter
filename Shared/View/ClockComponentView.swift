983fb43c058368fc9cbc0d3a142501e0f6564cea
linshaolong5240 <634205468@qq.com>
2021-06-29 19:32:56 +0800

    update

--- Shared/View/ClockComponentView.swift
+++ Shared/View/ClockComponentView.swift
@@ -208,13 +208,14 @@ struct ClockTemplateCornerGaugeImageView<Content: View>: View {
     }
     
     var body: some View {
-        ZStack(alignment: position.alignment) {
-            content(percent)
-                .padding(position.cornerPadding, 6)
-            GeometryReader { geometry in
-                let minLength = min(geometry.size.width, geometry.size.height)
-                let radius = minLength * 0.46
-                let strokeLineWidth = minLength * 0.03
+        GeometryReader { geometry in
+            let minLength = min(geometry.size.width, geometry.size.height)
+            let radius = minLength * 0.46
+            let strokeLineWidth = minLength * 0.03
+            ZStack(alignment: position.alignment) {
+                content(percent)
+                    .frame(width: minLength / 12.0, height: minLength / 12.0, alignment: .center)
+                    .frame(width: minLength / 5.0, height: minLength / 5.0, alignment: .center)
                 ZStack {
                     ZStack {
                         Path { path in
@@ -273,16 +274,19 @@ struct ClockTemplateCornerGaugeTextView: View {
     }
     
     var body: some View {
-        ZStack(alignment: position.alignment) {
-            text
-                .font(.system(size: 12))
-                .foregroundColor(.white)
-                .rotationEffect(position.textAngle)
-                .padding(position.cornerPadding, 10)
-            GeometryReader { geometry in
-                let minLength = min(geometry.size.width, geometry.size.height)
-                let radius = minLength * 0.46
-                let strokeLineWidth = minLength * 0.03
+        
+        GeometryReader { geometry in
+            ZStack(alignment: position.alignment) {
+            let minLength = min(geometry.size.width, geometry.size.height)
+            let radius = minLength * 0.46
+            let strokeLineWidth = minLength * 0.03
+                text
+                    .font(.system(size: 26))
+                    .minimumScaleFactor(0.4)
+                    .foregroundColor(.white)
+                    .rotationEffect(position.textAngle)
+                    .frame(width: minLength / 5.0, height: minLength / 5.0, alignment: .center)
+                    .frame(width: minLength / 4.0, height: minLength / 4.0, alignment: .center)
                 ZStack {
                     ZStack {
                         Path { path in
@@ -321,62 +325,8 @@ struct ClockTemplateCornerGaugeTextView: View {
 #if DEBUG
 struct ClockComponentView_Previews: PreviewProvider {
     static var previews: some View {
-        ZStack {
-            Color.black
-            ZStack {
-                Circle().fill(Color.white)
-                ClockMarkView(60) { index in
-                    Rectangle()
-                        .fill(Color.secondary)
-                        .frame(width: 1, height: 5, alignment: .center)
-                }
-                ClockMarkView(12) { index in
-                    Rectangle()
-                        .fill(Color.black)
-                        .frame(width: 1, height: 5, alignment: .center)
-                }
-                ClockMarkView(12, origin: true) { index in
-                    Text("\(index == 0 ? 12 : index)")
-                        .font(.system(size: 12))
-                        .foregroundColor(.black)
-                        .padding(.all, 5)
-                }
-                ClockNeedleView(Date(), for: .hour) {
-                    Rectangle()
-                        .fill(Color.blue)
-                        .frame(width: 5, height: 40, alignment: .center)
-                }
-                ClockNeedleView(Date(), for: .minute) {
-                    Rectangle()
-                        .fill(Color.green)
-                        .frame(width: 3, height: 50, alignment: .center)
-                }
-                ClockNeedleView(Date(), for: .second) {
-                    Rectangle()
-                        .fill(Color.pink)
-                        .frame(width: 3, height: 60, alignment: .center)
-                }
-                Circle()
-                    .frame(width: 5, height: 5, alignment: .center)
-            }
-            .padding()
-            Group {
-                ClockTemplateCornerGaugeImageView(percent: 0.5, color: .blue, position: .topLeading, leadingText: Text("0"), trailingText: Text("100")) { value in
-                    Image(systemName: "speaker.wave.2")
-                        .foregroundColor(.blue)
-                }
-                ClockTemplateCornerGaugeImageView(percent: 0.7, color: .orange, position: .topTrailing, leadingText: Text("0"), trailingText: Text("100")) { value in
-                    Image(systemName: "sun.max")
-                        .foregroundColor(.orange)
-                }
-//                ClockTemplateCornerGaugeTextView(percent: 0.25, color: .yellow, position: .topLeading, text: Text("63%"))
-//                ClockTemplateCornerGaugeTextView(percent: 0.25, color: .yellow, position: .topTrailling, text: Text("63%"))
-
-                ClockTemplateCornerGaugeTextView(percent: 0.25, color: .yellow, position: .bottomLeading, text: Text("63%"))
-                ClockTemplateCornerGaugeTextView(percent: 0.25, color: .green, position: .bottomTrailing, text: Text("63%"))
-            }
-        }
-        .previewContext(WidgetPreviewContext(family: .systemLarge))
+        WatchFaceWidgetView()
+            .previewContext(WidgetPreviewContext(family: .systemLarge))
     }
 }
 #endif
---
+++ Shared/View/Widget/WatchFaceWidgetView.swift
@@ -0,0 +1,83 @@
+//
+//  WatchFaceWidgetView.swift
+//  Widgeter
+//
+//  Created by 林少龙 on 2021/6/29.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct WatchFaceWidgetView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            let minLength = min(geometry.size.width, geometry.size.height)
+            ZStack {
+                Color.black
+                ZStack {
+                    Group {
+                        Circle().fill(Color.white)
+                        ClockMarkView(60) { index in
+                            Rectangle()
+                                .fill(Color.secondary)
+                                .frame(width: 1, height: 5, alignment: .center)
+                        }
+                        ClockMarkView(12) { index in
+                            Rectangle()
+                                .fill(Color.black)
+                                .frame(width: 1, height: 5, alignment: .center)
+                        }
+                        ClockMarkView(12, origin: true) { index in
+                            Text("\(index == 0 ? 12 : index)")
+                                .font(.system(size: 12))
+                                .foregroundColor(.black)
+                                .padding(.all, 5)
+                        }
+                        ClockNeedleView(Date(), for: .hour) {
+                            Rectangle()
+                                .fill(Color.blue)
+                                .frame(width: 5, height: 40, alignment: .center)
+                        }
+                        ClockNeedleView(Date(), for: .minute) {
+                            Rectangle()
+                                .fill(Color.green)
+                                .frame(width: 3, height: 50, alignment: .center)
+                        }
+                        ClockNeedleView(Date(), for: .second) {
+                            Rectangle()
+                                .fill(Color.pink)
+                                .frame(width: 3, height: 60, alignment: .center)
+                        }
+                        Circle()
+                            .frame(width: 5, height: 5, alignment: .center)
+                    }
+                    .padding()
+                    Group {
+                        ClockTemplateCornerGaugeImageView(percent: 0.5, color: .blue, position: .topLeading, leadingText: Text("0"), trailingText: Text("100")) { value in
+                            Image(systemName: "speaker.wave.2")
+                                .resizable()
+                                .foregroundColor(.blue)
+                        }
+                        ClockTemplateCornerGaugeImageView(percent: 0.7, color: .orange, position: .topTrailing, leadingText: Text("0"), trailingText: Text("100")) { value in
+                            Image(systemName: "sun.max")
+                                .foregroundColor(.orange)
+                        }
+                        //                ClockTemplateCornerGaugeTextView(percent: 0.25, color: .yellow, position: .topLeading, text: Text("63%"))
+                        //                ClockTemplateCornerGaugeTextView(percent: 0.25, color: .yellow, position: .topTrailling, text: Text("63%"))
+                        
+                        ClockTemplateCornerGaugeTextView(percent: 0.25, color: .yellow, position: .bottomLeading, text: Text("63%"))
+                        ClockTemplateCornerGaugeTextView(percent: 0.25, color: .green, position: .bottomTrailing, text: Text("63%"))
+                    }
+                }
+                .frame(width: minLength)
+            }
+        }
+    }
+}
+
+struct WatchFaceWidgetView_Previews: PreviewProvider {
+    static var previews: some View {
+        WatchFaceWidgetView()
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
